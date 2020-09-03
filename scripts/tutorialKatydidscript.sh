1a398151ae42eb3806da7546fff9dc62d2359aaf
Penny Slocum <penny.slocum@yale.edu>
2020-09-03 12:23:18 -0400

    Added scripts to run p8compute in singularity on Yale cluster.  Updated plotting notebooks and added source setup.sh to docker command scripts.

--- scripts/tutorialKatydidscript.sh
+++ scripts/tutorialKatydidscript.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 
-katydidversion=v2.17.2 # katydid version
-locustversion=v2.0.0 # locust version available in p8compute.
+katydidversion=v2.18.0 # katydid version
+locustversion=v2.1.2 # locust version available in p8compute.
+p8computeversion=v0.10.1 # p8compute version 
 katydiddir=/usr/local/p8/katydid/${katydidversion}  # in container.
 locustdir=/usr/local/p8/locust/${locustversion} # in container
+p8computedir=/usr/local/p8/compute/${p8computeversion} # in container.
 outputdir=/tmp/locust-tutorial/output # in container
 
 echo '#!/bin/bash' > katydidcommands.sh
-echo source ${katydiddir}/setup.sh >> katydidcommands.sh
+echo source ${p8computedir}/setup.sh >> katydidcommands.sh
 
 # Enter (or uncomment) a Katydid command below, with format "Katydid -c configfile.yaml" or "Katydid -c configfile.json"
 
 echo Katydid -c ${locustdir}/config/katydid.json -e ${outputdir}/locust_mc.egg -n 4 --brw-file ${outputdir}/basic.root --rsw-file ${outputdir}/spectrogram.root >> katydidcommands.sh
 
-#echo Katydid -c ${katydiddir}/config/ChannelAggregatorConfig.yaml -e ${outputdir}/locust_mc.egg --ch-sum.grid-size=5 -n 4 --brw-file ${outputdir}/agg_basic.root --rsw-file ${outputdir}/agg_spectrogram.root >> katydidcommands.sh
+#echo Katydid -c ${katydiddir}/config/ChannelAggregatorConfig.yaml -e ${outputdir}/locust_mc.egg --ch-sum.active-radius=0.1 -n 4 --brw-file ${outputdir}/agg_basic.root --rsw-file ${outputdir}/agg_spectrogram.root >> katydidcommands.sh
 
 # End Katydid commands.
 
--- scripts/tutorialLocustscript.sh
+++ scripts/tutorialLocustscript.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 p8hexbugdir=/tmp/hexbug # in container, bound to ~/p8tutorial/hexbug locally.
-locustversion=v2.1.1 # locust version
+locustversion=v2.1.2 # locust version
+p8computeversion=v0.10.1 # p8compute version 
 p8locustdir=/usr/local/p8/locust/${locustversion}  # in container.
+p8computedir=/usr/local/p8/compute/${p8computeversion} # in container.
 scriptsdir=~/p8tutorial/locust-tutorial/scripts # local scripts directory
 
 # Generate new command script:
 echo '#!/bin/bash' > ${scriptsdir}/locustcommands.sh
-echo source ${p8locustdir}/setup.sh >> ${scriptsdir}/locustcommands.sh
+echo source ${p8computedir}/setup.sh >> ${scriptsdir}/locustcommands.sh
 echo source ${p8locustdir}/bin/kasperenv.sh >> ${scriptsdir}/locustcommands.sh
 
 # Enter (or uncomment) one Locust command below, with format "LocustSim config=/path/to/config.json"
 
 # Example 1:
-echo LocustSim config=${p8locustdir}/config/LocustTestSignal.json >> ${scriptsdir}/locustcommands.sh
+#echo LocustSim config=${p8locustdir}/config/LocustTestSignal.json >> ${scriptsdir}/locustcommands.sh
 
 # Example 2:
 #echo LocustSim config=${p8locustdir}/config/LocustPlaneWaveTemplate.json >> ${scriptsdir}/locustcommands.sh
@@ -28,12 +30,12 @@ echo LocustSim config=${p8locustdir}/config/LocustTestSignal.json >> ${scriptsdi
 #echo LMCKassiopeia ${p8locustdir}/config/JustKassFieldMap.xml >> ${scriptsdir}/locustcommands.sh
 
 #Example 6:
-#echo LocustSim config=${p8hexbugdir}/Phase3/LocustPhase3Template.json >> ${scriptsdir}/locustcommands.sh
+echo LocustSim config=${p8hexbugdir}/Phase3/LocustPhase3Template.json >> ${scriptsdir}/locustcommands.sh
 
 # End Locust commands.
 
 chmod +x ${scriptsdir}/locustcommands.sh
 
 # Start the container, mount the shared directory(ies), and execute the Locust commands:
-docker run -it --rm -v ~/p8tutorial:/tmp -v ~/p8tutorial/locust-tutorial/output:${p8locustdir}/output project8/locust_mc /bin/bash -c /tmp/locust-tutorial/scripts/locustcommands.sh
+docker run -it --rm -v ~/p8tutorial:/tmp -v ~/p8tutorial/locust-tutorial/output:${p8locustdir}/output project8/p8compute /bin/bash -c /tmp/locust-tutorial/scripts/locustcommands.sh
 
