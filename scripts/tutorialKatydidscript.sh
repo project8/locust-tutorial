2599079cd95617665cac74f84c864be2ade07abb
Penny Slocum <penny.slocum@yale.edu>
2023-08-25 13:14:26 -0400

    Update tutorial scripts with current version numbers and some new examples.

--- scripts/tutorialKatydidscript.sh
+++ scripts/tutorialKatydidscript.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-
-katydidversion=v2.18.0 # katydid version
-locustversion=v2.2.0 # locust version available in p8compute.
+katydidversion=current # katydid version
 p8computeversion=v0.10.2 # p8compute version 
+locustversion=current
 katydiddir=/usr/local/p8/katydid/${katydidversion}  # in container.
 locustdir=/usr/local/p8/locust/${locustversion} # in container
 p8computedir=/usr/local/p8/compute/${p8computeversion} # in container.
@@ -16,12 +15,9 @@ echo source ${p8computedir}/setup.sh >> katydidcommands.sh
 
 echo Katydid -c ${locustdir}/config/katydid.json -e ${outputdir}/locust_mc.egg -n 4 --brw-file ${outputdir}/basic.root --rsw-file ${outputdir}/spectrogram.root >> katydidcommands.sh
 
-#echo Katydid -c ${katydiddir}/config/ChannelAggregatorConfig.yaml -e ${outputdir}/locust_mc.egg --ch-sum.active-radius=0.1 -n 4 --brw-file ${outputdir}/agg_basic.root --rsw-file ${outputdir}/agg_spectrogram.root >> katydidcommands.sh
-
 # End Katydid commands.
 
 chmod +x katydidcommands.sh
-
 
 # Start the container, mount the shared directory, and execute the Katydid commands.
 docker run -it --rm -v ~/p8tutorial:/tmp project8/p8compute /bin/bash -c /tmp/locust-tutorial/scripts/katydidcommands.sh
--- scripts/tutorialLocustscript.sh
+++ scripts/tutorialLocustscript.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
+# EDIT THIS PATH ONLY #######################
+tutorialdir=~/p8tutorial # path to locust-tutorial directory on local machine.
+#############################################
+
 p8hexbugdir=/tmp/hexbug # in container, bound to ${tutorialdir}/hexbug locally.
-locustversion=v2.2.0 # locust version
-p8computeversion=v0.10.2 # p8compute version 
-p8locustdir=/usr/local/p8/locust/${locustversion}  # in container.
-p8computedir=/usr/local/p8/compute/${p8computeversion} # in container.
-tutorialdir=~/p8tutorial # path to locust-tutorial directory on local machine.  
+locustversion=current # locust version
+locusttag=latest
+p8locustdir=/usr/local/p8/locust/${locustversion}  # in container
+kassiopeiadir=/usr/local/p8/kassiopeia/current
+commondir=/usr/local/p8/common/${commonversion} # in container.
+
 
 
 # Generate new command script:
 echo '#!/bin/bash' > ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
-echo source ${p8computedir}/setup.sh >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
-echo source ${p8locustdir}/bin/kasperenv.sh >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
+echo source ${p8locustdir}/setup.sh >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
+echo source ${kassiopeiadir}/bin/kasperenv.sh >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
-# Enter (or uncomment) one Locust command below, with format "LocustSim config=/path/to/config.json"
+# Enter (or just uncomment) one Locust command below, with format "LocustSim config=/path/to/config.json"
 
-# Example 1:
-echo LocustSim config=${p8locustdir}/config/LocustTestSignal.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
+# Example 0:  Test signal.
+#echo LocustSim config=${p8locustdir}/config/LocustTestSignal.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
-# Example 2:
-#echo LocustSim config=${p8locustdir}/config/LocustPlaneWaveTemplate.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
+# Example 1:  Test signal plus noise.
+#echo LocustSim config=${p8locustdir}/config/LocustSignalPlusNoise.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
-#Example 3:
-#echo LocustSim config=${p8locustdir}/config/LocustMagDipoleAntennaTemplate.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
+# Example 2:  25.9 GHz cavity (CCA)
+#echo LocustSim config=${p8locustdir}/config/LocustCavityCCA.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
-#Example 4:  
+# Example 3:  WR42 waveguide
+#echo LocustSim config=${p8locustdir}/config/LocustWaveguideTemplate.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
+
+# Example 4:  LW free space example
 #echo LocustSim config=${p8locustdir}/config/LocustFreeSpaceTemplate.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
-#Example 5:
+# Example 5:  Generate B field lines in Kassiopeia.
 #echo LMCKassiopeia ${p8locustdir}/config/JustKassFieldMap.xml >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
-
-#Example 6:
-#echo LocustSim config=${p8hexbugdir}/Phase3/LocustPhase3Template.json >> ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
 # End Locust commands.
 
 chmod +x ${tutorialdir}/locust-tutorial/scripts/locustcommands.sh
 
 # Start the container, mount the shared directory(ies), and execute the Locust commands:
-docker run -it --rm -v ${tutorialdir}:/tmp -v ${tutorialdir}/locust-tutorial/output:${p8locustdir}/output project8/p8compute /bin/bash -c /tmp/locust-tutorial/scripts/locustcommands.sh
+docker run -it --rm -v ${tutorialdir}:/tmp -v ${tutorialdir}/locust-tutorial/output:${p8locustdir}/output ghcr.io/project8/locust_mc:${locusttag} /bin/bash -c /tmp/locust-tutorial/scripts/locustcommands.sh
 
